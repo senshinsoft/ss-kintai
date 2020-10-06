d01ceb455825e34e732c16de5201934896c0cc22
k.takada <k.takada@senshin-soft.co.jp>
2020-10-06 17:27:24 +0900

    commit

---
+++ KintaiKanri/src/test/java/jp/co/senshinsoft/KintaiKanriApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.senshinsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiKanriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
