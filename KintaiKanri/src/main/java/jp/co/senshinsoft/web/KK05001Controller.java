aab765cf71389c0944e8d99af1f17d77b6af5694
s.fukamachi <s.fukamachi@senshin-soft.co.jp>
2020-11-25 16:06:47 +0900

    パスワード更新から180日以上経過していた場合にパスワード変更画面へ遷移する処理の実装

--- KintaiKanri/src/main/java/jp/co/senshinsoft/web/KK05001Controller.java
+++ KintaiKanri/src/main/java/jp/co/senshinsoft/web/KK05001Controller.java
@@ -1,12 +1,14 @@
 package jp.co.senshinsoft.web;
 
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.co.senshinsoft.auth.GetLoginUserDetails;
 
@@ -21,33 +23,53 @@ public class KK05001Controller {
 	public KK06001Form changePasswordForm() {
 		return new KK06001Form();
 	}
-	
+
 	/**
-	 * メニュー画面を表示する
-	 * 初回ログインとパスワード更新後180日経過していた場合はパスワード変更画面へ遷移
+	 * メニュー画面を表示する 初回ログインとパスワード更新後180日経過していた場合はパスワード変更画面へ遷移
+	 * 
 	 * @return KK05001、KK06001のパス
 	 */
 	@RequestMapping(value = "/menu")
 	public String menuInput(Model model) {
-		if(userInfo.getLoginUser().getPassUpdDate() != null) {
-		model.addAttribute("screenName", "メニュー");
-		model.addAttribute("userName",userInfo.getLoginUser().getSei()+" "+userInfo.getLoginUser().getMei() );
-		model.addAttribute("userInfo", userInfo.getLoginUser());	
-		return "KK05001";
-	}
+		if (userInfo.getLoginUser().getPassUpdDate() != null) {
+			SimpleDateFormat smdfmt = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar DBCalendar = Calendar.getInstance();
+			Calendar nowCalendar = Calendar.getInstance();
+			Date passUpdDate = new Date();
+			try {
+				passUpdDate = smdfmt.parse(userInfo.getLoginUser().getPassUpdDate());
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			DBCalendar.setTime(passUpdDate);
+			nowCalendar.add(Calendar.DAY_OF_MONTH, -180);
+			if (DBCalendar.before(nowCalendar)) {
+				model.addAttribute("screenName", "パスワード変更");
+				model.addAttribute("userName",userInfo.getLoginUser().getSei() + " " + userInfo.getLoginUser().getMei());
+				model.addAttribute("userInfo", userInfo.getLoginUser());
+				return "KK06001";
+			}
+			System.out.println((nowCalendar.getTime()));
+
+			model.addAttribute("screenName", "メニュー");
+			model.addAttribute("userName", userInfo.getLoginUser().getSei() + " " + userInfo.getLoginUser().getMei());
+			model.addAttribute("userInfo", userInfo.getLoginUser());
+			return "KK05001";
+		}
 		model.addAttribute("screenName", "パスワード変更");
 		model.addAttribute("userName", userInfo.getLoginUser().getSei() + " " + userInfo.getLoginUser().getMei());
 		model.addAttribute("userInfo", userInfo.getLoginUser());
-		return 	"KK06001";	
+		return "KK06001";
 	}
-	
-	@RequestMapping(value = "/menuConf", params="monthlyList")
-	public String inputWorkReport(){
+
+	@RequestMapping(value = "/menuConf", params = "monthlyList")
+	public String inputWorkReport() {
 		return "redirect:/monthlyList";
 	}
-	@RequestMapping(value = "/menuConf", params="pass")
-	public String passChange(){	
+
+	@RequestMapping(value = "/menuConf", params = "pass")
+	public String passChange() {
 		return "redirect:/enterKK06001";
 	}
-		
+
 }
